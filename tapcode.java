d493bd0a29ff3e213123116fe3dda5913c7d8b6f
tranduchieu08072006-hash <tranduchieu08072006@gmail.com>
2025-09-15 20:59:32 -0400

    test

--- tapcode.java
+++ tapcode.java
@@ -1,5 +1,9 @@
 public class tapcode {
      public static void main(String[] args) {
         System.out.println(" toi muon tro thanh sieu nhan " );
+          int a = 8; 
+          int m = 1000; 
+          System.out.println("ket qua la " + ( a + m));
+
      }
 }
